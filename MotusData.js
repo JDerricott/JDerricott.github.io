d3616f2363e531418eeac43b651fce6085c7db37
jeffderricott <jeffderricott@gmail.com>
2019-02-28 13:29:54 -0700

    fixed typos in css and js files

--- MotusData.js
+++ MotusData.js
@@ -1,9 +1,11 @@
 require(["esri/Map", 
 	"esri/views/MapView",
-	"esri/layers/MapImageLayer"], function(
+	"esri/layers/MapImageLayer",
+	"esri/layers/support/Sublayer"], function(
   Map,
   MapView,
-  MapImageLayer
+  MapImageLayer,
+  Sublayer
 ) {
   var map = new Map({
     basemap: "streets-navigation-vector"
@@ -18,6 +20,7 @@ require(["esri/Map",
   // points to the states layer in a service storing U.S. census data
   var layer = new MapImageLayer({
     url: "https://geoserver2.byu.edu/arcgis/rest/services/Motus/MOTUSServer/MapServer"
+    sublayers: [ {id: 0}]
   });
   map.add(layer);  // adds the layer to the map
 });
